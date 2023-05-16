b759fd62469fe838e3dcd375f17f6582871a0c8c
Santiago Lataza <santiagolataza@Santiagos-MBP.fibertel.com.ar>
2023-05-16 00:06:50 -0300

    parametrized driver with testngXML

--- src/test/java/org/seleniumTest/testng/search/GoogleSearchTest.java
+++ src/test/java/org/seleniumTest/testng/search/GoogleSearchTest.java
@@ -1,14 +1,15 @@
-package org.seleniumTest;
+package org.seleniumTest.testng.search;
 
 import org.seleniumTest.pageObjects.GoogleHomePage;
 import org.seleniumTest.pageObjects.GoogleResultPage;
+import org.seleniumTest.testng.BaseTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 public class GoogleSearchTest extends BaseTest {
 
     @Test (description="Verify google search by pressing enter after declaring the search term in the text box.")
-    void googleHomePage_searchByEnter() {
+    public void googleHomePage_searchByEnter() {
         GoogleHomePage googleHomePage = new GoogleHomePage(driver).goTo();
         GoogleResultPage googleResultPage = googleHomePage.searchByEnter("panda");
         assertTrue(googleResultPage.isCurrentPage());
@@ -16,7 +17,7 @@ public class GoogleSearchTest extends BaseTest {
     }
 
     @Test (description="Verify google search by submitting the search text box after declaring the search term.")
-    void googleHomePage_searchBySubmit() {
+    public void googleHomePage_searchBySubmit() {
         GoogleHomePage googleHomePage = new GoogleHomePage(driver).goTo();
         GoogleResultPage googleResultPage = googleHomePage.searchBySubmit("panda");
         assertTrue(googleResultPage.isCurrentPage());
@@ -24,7 +25,7 @@ public class GoogleSearchTest extends BaseTest {
     }
 
     @Test (description="Verify google search by submitting the search button after declaring the term in the text box.")
-    void googleHomePage_searchByButton() {
+    public void googleHomePage_searchByButton() {
         GoogleHomePage googleHomePage = new GoogleHomePage(driver).goTo();
         GoogleResultPage googleResultPage = googleHomePage.searchByButton("panda");
         assertTrue(googleResultPage.isCurrentPage());
