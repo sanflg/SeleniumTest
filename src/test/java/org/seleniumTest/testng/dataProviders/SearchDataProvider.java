8f92fac3ccceb5d5663709942ad415bdca98ee2e
Santiago Lataza <santiagolataza@Santiagos-MBP.fibertel.com.ar>
2023-05-21 03:50:09 -0300

    allure improvements

--- src/test/java/org/seleniumTest/testng/dataProviders/SearchDataProvider.java
+++ src/test/java/org/seleniumTest/testng/dataProviders/SearchDataProvider.java
@@ -26,16 +26,5 @@ public class SearchDataProvider {
                 {"enter", "staircase"}
         };
     }
-
-    // TODO 4 there is a less ugly way to do a product of 1D matrices wih daaProviders?
-//    Object[][] matrix = new Object[searches.length + terms.length + 1][2];
-//        for (int i = 0; i < matrix.length;) {
-//        for (int j = 0; j < 2; j++) {
-//            matrix[i][0] = searches[i/2];
-//            matrix[i][1] = terms[j];
-//            i++;
-//        }
-//    }
-//        return matrix;
 }
 
