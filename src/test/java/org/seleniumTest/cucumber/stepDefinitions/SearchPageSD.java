7bb5808b9a0c56a3b2fd94c3c356a204b7851e3d
Santiago Lataza <santiagolataza@Santiagos-MBP.fibertel.com.ar>
2023-05-21 03:10:24 -0300

    added pom versions

--- src/test/java/org/seleniumTest/cucumber/stepDefinitions/SearchPageSD.java
+++ src/test/java/org/seleniumTest/cucumber/stepDefinitions/SearchPageSD.java
@@ -4,8 +4,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.seleniumTest.pageObjects.SearchPage;
 
-//TODO 1 - Call @Step method with another @Step method and see what happens in logings
-//TODO 1 Learn Gherkin
+// TODO 1 - Call @Step method with another @Step method and see what happens in logings
+// TODO 1 Learn Gherkin
 public final class SearchPageSD {
     private final SearchPage searchPage = new SearchPage();
 
@@ -14,7 +14,7 @@ public final class SearchPageSD {
         searchPage.searchBy(search, term);
     }
 
-    //TODO 2 Investigate for abstract implementation for cucumber
+    // TODO 2 Investigate for abstract implementation for cucumber
     @Given("User is in search page")
     public void userIsInSearchPage() {
         searchPage.goTo();
